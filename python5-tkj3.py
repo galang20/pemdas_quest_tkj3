e0b044554f3adc503e92b2be26d04ffebd46e7b8
galang20 <112367414+galang20@users.noreply.github.com>
2022-09-01 20:18:54 -0700

    galang xtkj 3
    python5

---
+++ python5-tkj3.py
@@ -0,0 +1,43 @@
+#Rumus Persegi Sisi * sisi
+
+
+
+sisi1= 10
+sisi2=10
+luas_p = sisi1 * sisi2
+
+print("luas persegi adalah" , luas_p)
+
+
+#Rumus Segitiga A * T / 2
+
+alas = 20
+tinggi = 10
+luas_s = alas * tinggi
+
+print("luas segitiga adalah" , luas_s)
+
+
+
+#Rumus Jajar Genjang Alas * Tinggi
+
+alas = 10
+tinggi = 20
+luas_j = alas * tinggi
+
+print("luas jajar genjang adalah" , luas_j)
+
+
+#Rumus Lingkaran phi * r * r
+
+phi= 22/7
+r = 21
+luas_l = phi * r *r
+
+print("luas lingkaran adalah" , luas_l)
+
+
+print("\ni.luas persegi")
+sisi = int(input("maukan nilai sisi :  "))
+luas_p = sisi * sisi
+print("luas persegi adalah" , luas_p) 
