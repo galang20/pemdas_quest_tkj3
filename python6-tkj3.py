5dec50c124b0accbff833d729df2b7ab6373830c
galang20 <112367414+galang20@users.noreply.github.com>
2022-09-04 22:29:09 -0700

    Tugas Pemdas Galang Raditya P

---
+++ python6-tkj3.py
@@ -0,0 +1,24 @@
+#Dolar Ke Rupiah
+dolar = int(input("Masukan nilai dolar: "))
+rupiah = dolar * 15000
+
+print ("Dolar Ke Rupiah" , rupiah )
+
+
+#Yen Ke Rupiah
+yen = int(input("masukan nilai yen: " ))
+rupiah = yen * 106
+
+print ("Yen Ke Rupiah" , rupiah)
+
+
+#Euro Ke Won 
+won = int(input("Masukan nilai won: "))
+euro = won * 1357
+
+print ("Euro Ke Won" , won)
+
+
+
+
+
