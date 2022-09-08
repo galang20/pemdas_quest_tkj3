3225f3b65e3d346902be49209938f17318a4aa7d
galang20 <112367414+galang20@users.noreply.github.com>
2022-09-07 23:45:25 -0700

    galang xtkj3

---
+++ python-latihan.py
@@ -0,0 +1,39 @@
+# Rumus luas persegi
+print("\n.luas persegi")
+sisi = int(input("masukan nilai sisi"))
+luas_p = sisi * sisi
+
+print("luas persegi adalah",luas_p)
+
+#Rumus segitiga
+print("\n.luas segitiga")
+alas = int(input("masukan nilai alas:"))
+tinggi = int (input("masukan nilai tinggi: "))
+luas_s = alas * tinggi /2
+
+print("luas segitiga adalah",luas_s)
+
+#Rumus persegi panjang
+print("\n.luas persegi panjang")
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("maskan nilai lebar:"))
+luas_pp = panjang * lebar 
+
+print("luas persegi panjang adalah",luas_pp)
+
+#Rumus lingkaran 
+print("\n.luas lingkara")
+phi = 22/7
+r   = int(input("masukan nilai r: "))
+luas_l = phi * r
+
+print("luas lingkaran adalah",luas_l)
+
+#Rumus jajar genjang
+print("\n.luas jajar genjnag")
+alas = int(input("maskan nilai alas "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_j = alas * tinggi
+
+print("luas jajar genjang adalah",luas_j)
+
